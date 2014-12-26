3911293dbba03494fd911831da8ef395dc26fb80
James Provinciali <merigis@njmeadowlands.gov>
2014-12-26 10:38:29 -0500

    CSV data is now properly in arrays

--- main.js
+++ main.js
@@ -16,10 +16,15 @@ var options = {
             text: 'Height'
         }
     },
-    series: []
+    series: [{
+
+        name: 'Water Height',
+        data: []
+    }]
 };
 
-$.get('HAWKCSV.csv', function(data) {
+$.get('HAWKCSV.csv', function(data) 
+{
     // Split the lines
     var lines = data.split('\n'); // Splits based on the end of the line
     //console.log(lines);
@@ -31,87 +36,36 @@ $.get('HAWKCSV.csv', function(data) {
     var Ys = []; //holds the values
     //console.log(Ys);
 
-
-
-
-    $.each(lines, function(lineNo, line) { //indexes each lineNo = 0, line = value associated
+    $.each(lines, function(lineNo, line) //indexes each lineNo = 0, line = value associated
+    { 
         var items = line.split(','); //split by commas
         //console.log(items);
-        // header line containes categories
         
-        //console.log('Date/Time: ' + items[0]);
-        //console.log('Values: ' + items[1]);
-
         
-        if(items[0] != "Date"){
-            Xs.push(items[0]);
-            //console.log('X: ' + Xs);
-         } 
-
+        //console.log('Date/Time(x): ' + items[0]);
+        //console.log('Values(y): ' + items[1]);
         
-        if(items[1] != "Flood_Depth" ) {
-            Ys.push(items[1]);
-            //console.log('Y: ' + Ys);
-         }
-        
+        Xs.push(items[0]);
+        //console.log('X: ' + Xs);
+    
+        Ys.push(items[1]);
+        //console.log('Y: ' + Ys);
+        //console.log('Values(y): ' + items[1]);
     });
 
 
+    Xs.splice(0,1);  //removes first element
+    Ys.splice(0,1); 
+    Xs.splice(143,1); //removes last undefined element from array
+    Ys.splice(143,1); 
+    
+
     options.xAxis.categories.push(Xs);
     options.series.push(Ys);
-    console.log(Xs);
-    console.log(Ys);
+    //console.log(Xs);
+    //console.log(Ys);
 
-        
-        
-        
-            
-        
-          
-
-        
-        
-        /*
-        if (lineNo == 0) {
-            $.each(items, function(itemNo, item) { 
-                //console.log('item no:  ' + itemNo);
-                //console.log('item : '  + item);
-                //if (itemNo > 0) options.xAxis.categories.push(item);
-
-            });
-        }
-        */
-        
-        // the rest of the lines contain data with their name in the first 
-        // position
-        //else {
-            
-            
-            /*
-            var series = {
-                data: []
-            };
-            $.each(items, function(itemNo, item) {
-                //console.log('item no :' + itemNo);
-               //console.log('item :' + item);
-                if (itemNo == 0) {
-                    series.name = item;
-                } else {
-                    series.data.push(parseFloat(item));
-                }
-            });
-            
-            
-            options.series.push(series);
-        //}
-        */
-
-        
-
-        
-        
-    
-    
     // Create the chart
     var chart = new Highcharts.Chart(options);
+    //console.log(chart);
 });
